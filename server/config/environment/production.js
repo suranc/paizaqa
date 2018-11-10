292b3fdbae88692a4e9a008fb06b43394f77148c
Chris Suran <suranc1@nationwide.com>
2018-11-10 01:22:06 -0500

    Moving kube environment to production

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -4,21 +4,21 @@
 // =================================
 module.exports = {
   // Server IP
-  ip:     process.env.OPENSHIFT_NODEJS_IP ||
+  ip:     process.env.SERVER_IP ||
           process.env.IP ||
           undefined,
 
   // Server port
-  port:   process.env.OPENSHIFT_NODEJS_PORT ||
+  port:   process.env.SERVER_PORT ||
           process.env.PORT ||
           8080,
 
+  // Seed database on startup
+  seedDB: false,
+
   // MongoDB connection options
   mongo: {
-    uri:  process.env.MONGOLAB_URI ||
-          process.env.MONGOHQ_URL ||
-          process.env.OPENSHIFT_MONGODB_DB_URL +
-          process.env.OPENSHIFT_APP_NAME ||
+    uri:  process.env.MONGO_URI ||
           'mongodb://localhost/paizaqa'
   }
 };
