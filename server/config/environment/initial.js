bce7c2494e89fb8386c27aea2bed3e768a464a68
Chris Suran <suranc1@nationwide.com>
2018-11-09 23:49:01 -0500

    Adding inital and kube environment configs

---
+++ server/config/environment/initial.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// Production specific configuration
+// =================================
+module.exports = {
+  // Server IP
+  ip:     process.env.SERVER_IP ||
+          process.env.IP ||
+          undefined,
+
+  // Server port
+  port:   process.env.SERVER_PORT ||
+          process.env.PORT ||
+          8080,
+
+  // Seed database on startup
+  seedDB: true,
+
+  // MongoDB connection options
+  mongo: {
+    uri:  process.env.MONGO_URI ||
+          'mongodb://localhost/paizaqa'
+  }
+};
